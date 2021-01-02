dad50017b41979f84e48a2701085c0238279bf38
Sebastian <alu0101131108@ull.edu.es>
2021-01-02 20:13:05 +0000

    funsiona piola mas o menos

--- p03-openCV-gestos.py
+++ p03-openCV-gestos.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)   # '0' argument has to do with camera device selection.
+cap = cv2.VideoCapture(0)
 backSub = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
 
 if not cap.isOpened:
@@ -11,32 +11,31 @@ if not cap.isOpened:
 pt1 = (400, 100)
 pt2 = (600, 300)
 
-# Save video output at "./out.avi".
-# frame_width = int(cap.get(3))
-# frame_height = int(cap.get(4))
-# out = cv2.VideoWriter('out.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20, (frame_width, frame_height))
-
 while (True):
     ret, frame = cap.read()
     if not ret:
         exit(0)
 
-    fgMask = backSub.apply(frame)
-
     frame = cv2.flip(frame, 1)
 
-##    roi = frame[pt1[1]:pt2[1], pt1[0]:pt2[0], : ].copy()
-##    cv2.rectangle(frame, pt1, pt2, (255,0,0))
-##    cv2.imshow('ROI', roi)
-    cv2.imshow('Foreground Mask', fgMask)
+    roi = frame[pt1[1]:pt2[1], pt1[0]:pt2[0], : ].copy()
+    cv2.rectangle(frame, pt1, pt2, (255,0,0))
+    
+    fgMask = backSub.apply(roi,1)
 
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    ret, bw = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    cv2.drawContours(roi, contours, -1, (0, 255, 0), 3)
+
+
+    cv2.imshow('ROI', roi)
+    cv2.imshow('Foreground Mask', fgMask)
     cv2.imshow('frame', frame)
-#     out.write(frame)
 
     keyboard = cv2.waitKey(1)
     if keyboard & 0xFF == ord('q'):
         break
 
 cap.release()
-# out.release()
 cv2.destroyAllWindows()
